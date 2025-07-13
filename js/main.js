55f899206cc2e530a5017352a619ce630d7846f7
lululthfh <punyalulu05@gmail.com>
2025-07-13 22:00:45 +0700

    feat: add function handle restart game to reset the game

--- js/main.js
+++ js/main.js
@@ -81,3 +81,11 @@ function handlePlayerChange() {
   currentPlayer = currentPlayer === "X" ? "O" : "X";
   statusDisplay.innerHTML = currentPlayerTurn();
 }
+
+function handleRestartGame() {
+  gameActive = true;
+  currentPlayer = "X";
+  gameState = ["", "", "", "", "", "", "", "", ""];
+  statusDisplay.innerHTML = currentPlayerTurn();
+  document.querySelectorAll(".cell").forEach((cell) => (cell.innerHTML = ""));
+}
