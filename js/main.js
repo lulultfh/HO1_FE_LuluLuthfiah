531fe505ad6c467512814cd40d16f4074b9f1022
lululthfh <punyalulu05@gmail.com>
2025-07-13 21:56:33 +0700

    feat: detect winning moves and handle draw situation

--- js/main.js
+++ js/main.js
@@ -47,3 +47,32 @@ const winningConditions = [
   [0, 4, 8],
   [2, 4, 6],
 ];
+
+function handledResultValidation() {
+  let roundWon = false;
+  for (let i = 0; i <= 7; i++) {
+    const winCondition = winningConditions[i];
+    let a = gameState[winCondition[0]];
+    let b = gameState[winCondition[1]];
+    let c = gameState[winCondition[2]];
+    if (a === "" || b === "" || c === "") {
+      continue;
+    }
+    if (a === b && b === c) {
+      roundWon = true;
+      break;
+    }
+  }
+  if (roundWon) {
+    statusDisplay.innerHTML = winningMessage();
+    gameActive = false;
+    return;
+  }
+  let roundDraw = !gameState.includes("");
+  if (roundDraw) {
+    statusDisplay.innerHTML = drawMessage();
+    gameActive = false;
+    return;
+  }
+  handlePlayerChange();
+}
